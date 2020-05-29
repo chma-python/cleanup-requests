77da5ad31cd612846a3f57d42123f0cca0e6e15c
chma-python <steadynappin@gmail.com>
2020-05-28 20:33:33 -0400

    unscripting

--- main.py
+++ main.py
@@ -3,68 +3,68 @@ import os
 import json
 import requests
 
-def cleanupRequests(n=10):
+# def cleanupRequests(n=10):
     """
     Finds the last n records in the Requests table where "Status" = "Request Complete" that were last modified at least 30 days ago, erases the "Message" and "First Name" fields and deletes any recordings associated with the call.
     """
 
     # formula for filtering data from airtable
-    formula = 'AND(DATETIME_DIFF(NOW(), {Last Modified}, "days") > 30, Status = "Request Complete")'
+formula = 'AND(DATETIME_DIFF(NOW(), {Last Modified}, "days") > 30, Status = "Request Complete")'
 
-    # airtable query
-    headers = {"Authorization": "Bearer {}".format(os.environ['AIRTABLE_AUTH_TOKEN'])}
-    params =  params = {
-            'maxRecords': 10,
-            'view': 'All Requests + Data',
-            'sortField':'Last Modified',
-            'sortDirection': 'asc',
-            'filterByFormula': formula
+# airtable query
+headers = {"Authorization": "Bearer {}".format(os.environ['AIRTABLE_AUTH_TOKEN'])}
+params =  params = {
+        'maxRecords': 10,
+        'view': 'All Requests + Data',
+        'sortField':'Last Modified',
+        'sortDirection': 'asc',
+        'filterByFormula': formula
 
-        }
+    }
 
 
-    r = requests.get(os.environ['PROD_URL'], headers=headers, params=params)
-    
-    # if status code is good ...
-    if r.status_code == 200:
+r = requests.get(os.environ['PROD_URL'], headers=headers, params=params)
 
-        # instantiate twilio client
-        client = Client(os.environ['ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
+# if status code is good ...
+if r.status_code == 200:
 
-        # iterate through records
-        for record in r.json()['records']:
+    # instantiate twilio client
+    client = Client(os.environ['ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
 
-            data = {
-                'fields':
-                  {'Message': "",
-                   'First Name': ""
-                  }
-                }
+    # iterate through records
+    for record in r.json()['records']:
 
-            # patch the requisite fields
-            r = requests.patch(
-                os.environ['PROD_URL'] + record['id'] , headers=headers, json=data
-            )
+        data = {
+            'fields':
+              {'Message': "",
+               'First Name': ""
+              }
+            }
 
-            # erase the recordings associated with the call SID
-            call_sid = record['fields']['Twilio Call Sid']
-            call = client.calls(call_sid).fetch()
+        # patch the requisite fields
+        r = requests.patch(
+            os.environ['PROD_URL'] + record['id'] , headers=headers, json=data
+        )
 
-            for recording_sid in call.recordings.list():
-                client.recordings(recording_sid).delete()
+        # erase the recordings associated with the call SID
+        call_sid = record['fields']['Twilio Call Sid']
+        call = client.calls(call_sid).fetch()
 
-            # confirm deletion
-            r = requests.get(os.environ['PROD_URL'] + record['id'], headers=headers)
-            call = client.calls(call_sid).fetch()
+        for recording_sid in call.recordings.list():
+            client.recordings(recording_sid).delete()
 
-            if all([r.status_code == 200, 
-                   'Message' not in r.json().keys(), 
-                   'First Name' not in r.json().keys(),
-                   len(call.recordings.list()) == 0]):
-                print('succesfully deleted')
-                
-            else:
-                print('error')
+        # confirm deletion
+        r = requests.get(os.environ['PROD_URL'] + record['id'], headers=headers)
+        call = client.calls(call_sid).fetch()
 
-if __name__ == "__main__":
-    cleanupRequests()
+        if all([r.status_code == 200, 
+               'Message' not in r.json().keys(), 
+               'First Name' not in r.json().keys(),
+               len(call.recordings.list()) == 0]):
+            print('succesfully deleted')
+            
+        else:
+            print('error')
+
+# if __name__ == "__main__":
+#     cleanupRequests()
